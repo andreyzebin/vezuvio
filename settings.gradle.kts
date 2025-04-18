04633687d482ad0ae1b2a33b121d6489ee760837
andrey.zebin <andrey.zebin@gmail.com>
2025-04-18 17:21:33 +0300

    fix tray -> vesuvius

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,25 +4,10 @@
  * The settings file is used to specify which projects to include in your build.
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/8.13/userguide/multi_project_builds.html in the Gradle documentation.
  */
-pluginManagement {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        maven("https://jitpack.io")
-        gradlePluginPortal()
-    }
-}
+
 
 // use only for home/dev use - e.q.: no enterprise repositories configuration
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        maven(url = "https://jitpack.io")
-        gradlePluginPortal()
-    }
-}
+
 
 
 
