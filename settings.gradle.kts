a0f9f77429a19a75171b6331e202ade98641d9e2
andrey.zebin <andrey.zebin@gmail.com>
2025-04-18 17:07:32 +0300

    fix tray -> vesuvius

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,10 +4,13 @@
  * The settings file is used to specify which projects to include in your build.
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/8.13/userguide/multi_project_builds.html in the Gradle documentation.
  */
-
-plugins {
-    // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    // id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven("https://jitpack.io")
+        gradlePluginPortal()
+    }
 }
 
 // use only for home/dev use - e.q.: no enterprise repositories configuration
@@ -21,14 +24,7 @@ dependencyResolutionManagement {
     }
 }
 
-pluginManagement {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        maven("https://jitpack.io")
-        gradlePluginPortal()
-    }
-}
+
 
 rootProject.name = "vezuvio"
 include("app")
