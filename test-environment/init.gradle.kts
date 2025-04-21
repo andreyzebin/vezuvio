6821838648acca432377a0265bdc534dc18536ee
andrey.zebin <andrey.zebin@gmail.com>
2025-04-21 09:40:28 +0300

    fix tray -> vesuvius

--- test-environment/init.gradle.kts
+++ test-environment/init.gradle.kts
@@ -2,13 +2,19 @@ settingsEvaluated {
     pluginManagement {
         repositories.clear()
         repositories {
+            mavenLocal()
+            mavenCentral()
             maven("https://jitpack.io")
+            gradlePluginPortal()
         }
     }
     dependencyResolutionManagement {
         repositories.clear()
         repositories {
+            mavenLocal()
+            mavenCentral()
             maven("https://jitpack.io")
+            gradlePluginPortal()
         }
     }
 }
