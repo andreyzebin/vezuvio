3d8fe2054511d843a4c1bd3554e79631a7c984b0
andrey.zebin <andrey.zebin@gmail.com>
2025-05-16 22:10:07 +0300

    added html out format

---
+++ app/src/main/java/io/github/zebin/Daemon.java
@@ -0,0 +1,4 @@
+package io.github.zebin;
+
+public class Daemon {
+}
---
+++ app/src/test/java/io/github/zebin/DaemonTest.java
@@ -0,0 +1,75 @@
+package io.github.zebin;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.StandardProtocolFamily;
+import java.net.UnixDomainSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DaemonTest {
+
+
+    @Test
+    public void test() throws IOException, InterruptedException {
+        Path socketPath = Path
+                .of(System.getProperty("user.home"))
+                .resolve("vezuvio.socket");
+
+        try {
+            UnixDomainSocketAddress socketAddress = UnixDomainSocketAddress.of(socketPath);
+
+            ServerSocketChannel serverChannel = ServerSocketChannel.open(StandardProtocolFamily.UNIX);
+            serverChannel.bind(socketAddress);
+
+            send(socketAddress);
+
+            SocketChannel serverAccept = serverChannel.accept();
+
+            while (true) {
+                readSocketMessage(serverAccept)
+                        .ifPresent(msg -> System.out.printf("[Client message] %s", msg));
+                Thread.sleep(100);
+            }
+        } finally {
+            Files.deleteIfExists(socketPath);
+        }
+
+    }
+
+    private static void send(UnixDomainSocketAddress socketAddress) throws IOException {
+        SocketChannel channel = SocketChannel
+                .open(StandardProtocolFamily.UNIX);
+        channel.connect(socketAddress);
+        String message = "Hello from Baeldung Unix domain socket article";
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        buffer.clear();
+        buffer.put(message.getBytes());
+        buffer.flip();
+        while (buffer.hasRemaining()) {
+            channel.write(buffer);
+        }
+    }
+
+    private Optional<String> readSocketMessage(SocketChannel channel) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        int bytesRead = channel.read(buffer);
+        if (bytesRead < 0)
+            return Optional.empty();
+
+        byte[] bytes = new byte[bytesRead];
+        buffer.flip();
+        buffer.get(bytes);
+        String message = new String(bytes);
+        return Optional.of(message);
+    }
+
+
+}
